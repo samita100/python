6f2cc2b594eac3d216362f8bbdd77b3070af41b0
samita100 <66769151+samita100@users.noreply.github.com>
2020-06-12 10:54:58 +0545

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,31 @@
+python3 main.py
+
+sleep 329
+
+python3 support.py
+
+sleep 407
+
+python3 main.py
+
+sleep 409
+
+python3 support.py
+
+sleep 399
+
+python3 main.py
+
+sleep 401
+
+python3 support.py
+
+sleep 404
+
+python3 main.py
+
+sleep 340
+
+python3 support.py
+
+sleep 413
